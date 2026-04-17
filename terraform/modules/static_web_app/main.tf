b83e8d15c94650f4d365c31eb2a665d738a4774b
Catarina Trancoso <catarinamftrancoso@gmail.com>
2026-04-17 21:54:25 +0100

    update resource

--- terraform/modules/static_web_app/main.tf
+++ terraform/modules/static_web_app/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_static_web_app" "this" {
+resource "azurerm_static_web_app" "frontend" {
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
